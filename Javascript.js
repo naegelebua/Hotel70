b215bc51c9a79dd3adff4be8fe71cec6f8af0308
Anika <anikaholzner07@gmail.com>
2025-09-30 09:03:39 +0200

    inital commit

---
+++ Javascript.js
@@ -0,0 +1,14 @@
+
+function toggleMenu() {
+    const menu = document.getElementById('sideMenu');
+    menu.classList.toggle('open');
+}
+
+// Stelle sicher, dass man beim Reload wieder ganz oben ist
+window.addEventListener("load", function() {
+    
+    if (window.location.hash) {
+        history.replaceState("", document.title, window.location.pathname + window.location.search);
+        window.scrollTo(0, 0); // sofort ganz nach oben scrollen
+    }
+});
